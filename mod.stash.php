79a67d50c902526b9704dd6d3fcb39e12c946025
Eli Van Zoeren <eli@elivz.com>
2013-10-11 11:32:10 -0400

    I'm an idiot

--- mod.stash.php
+++ mod.stash.php
@@ -1274,7 +1274,7 @@ class Stash {
 			Yes! {my_string} is empty
 		{/if}
 		--------------------------------------------------------- */
-		return ! $this->not_empty($string);
+		return $this->not_empty($string) == 0 ? 1 : 0;
 	}
 	
 	// ---------------------------------------------------------
